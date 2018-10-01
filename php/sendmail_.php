2883d5ae9872bdc7c9a309aeb11a600dae25f5c0
mvn2004 <mvn2004@mail.ru>
2018-10-01 09:10:20 +0300

    вторая почта

---
+++ php/sendmail_.php
@@ -0,0 +1,34 @@
+<?php
+  //Проверка Поля ИМЯ
+  if(trim($_POST['Name']) == '') {
+  $hasError = true;
+  } else {
+  $name = trim($_POST['Name']);
+  }
+ //Проверка правильности ввода EMAIL
+  if(trim($_POST['E-mail']) == '')  {
+  $hasError = true;
+  } else if (!eregi("^[A-Z0-9._%-]+@[A-Z0-9._%-]+\.[A-Z]{2,4}$", trim($_POST['E-mail']))) {
+  $hasError = true;
+  } else {
+  $email = trim($_POST['E-mail']);
+  }
+ //Проверка наличия телефона
+  if(trim($_POST['phone']) == '') {
+  $hasError = true;
+  } else {
+  $phone = trim($_POST['phone']);
+  }
+ //Если ошибок нет, отправить email
+  if(!isset($hasError)) {
+  $emailTo = 'medstarsolutions2017@gmail.com';
+  $body = "Name: $name \n\nEmail: $email \n\nPhone: $phone";
+  $headers = 'From: IDIS <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email ."r";
+  $headers .= "Content-type: text/plain; charset='utf-8'r";
+ mail($emailTo, $name, $body, $headers);
+  $emailSent = true;
+  }
+  $redirect = isset($_SERVER['HTTP_REFERER'])? $_SERVER['HTTP_REFERER']:'../index.html';
+  header("Location: $redirect");
+  exit();
+  ?>
