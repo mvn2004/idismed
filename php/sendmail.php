3b2d2ed608482fffa3fd117cd170039a8e457e7d
mvn2004 <mvn2004@mail.ru>
2018-10-02 10:50:18 +0300

    Исправление PHP и адаптивность нового блока

--- php/sendmail.php
+++ php/sendmail.php
@@ -1,32 +1,36 @@
 <?php
   //Проверка Поля ИМЯ
   if(trim($_POST['Name']) == '') {
-  $hasError = true;
-  } else {
-  $name = trim($_POST['Name']);
-  }
- //Проверка правильности ввода EMAIL
+		$hasError = true;
+		} else {
+		$name = trim($_POST['Name']);
+	}
+	//Проверка правильности ввода EMAIL
   if(trim($_POST['E-mail']) == '')  {
-  $hasError = true;
-  } else {
-  $email = trim($_POST['E-mail']);
-  }
- //Проверка наличия телефона
+		$hasError = true;
+		} else {
+		$email = trim($_POST['E-mail']);
+	}
+	//Проверка наличия телефона
   if(trim($_POST['Phone']) == '') {
-  $hasError = true;
-  } else {
-  $phone = trim($_POST['Phone']);
-  }
- //Если ошибок нет, отправить email
+		$hasError = true;
+		} else {
+		$phone = trim($_POST['Phone']);
+	}
+	//Если ошибок нет, отправить email
   if(!isset($hasError)) {
-  $emailTo = 'nmt@techgroup.com.ua';
-  $body = "Name: $name \n\nE-mail: $email \n\nPhone: $phone";
-  $headers = "Content-type: text/plain; charset='utf-8'r";
-  $headers .= 'From: IDIS <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email ."r";
- mail($emailTo, $name, $body, $headers);
-  $emailSent = true;
-  }
+		$emailTo = 'nmt@techgroup.com.ua';
+		$body = "Name: $name \n\nE-mail: $email \n\nPhone: $phone";
+		$headers = "Content-type: text/plain; charset=utf-8 \r\n";
+		$headers .= 'From: IDIS <'.$emailTo.'>' . "\r\n" . 'Reply-To: ' . $email ."r";
+		//Перекодировка сообщения для правильного отображения имени на русском языке
+		$preferences = array("input-charset" => "UTF-8", "output-charset" => "UTF-8");
+		$encoded_subject = iconv_mime_encode("Message from IDIS", $name, $preferences);
+		
+		mail($emailTo, $encoded_subject, $body, $headers);
+		$emailSent = true;
+	}
   $redirect = isset($_SERVER['HTTP_REFERER'])? $_SERVER['HTTP_REFERER']:'../index.html';
   header("Location: $redirect");
   exit();
-  ?>
+?>
